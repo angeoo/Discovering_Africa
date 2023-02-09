a5babcb5115d4c77226d5ab4b80191242f83b858
Ange <amg-marie-adingra.konian@epita.fr>
2023-02-09 23:12:21 +0100

    makefile to make

--- src/decode/utils.c
+++ src/decode/utils.c
@@ -6,11 +6,12 @@ int module_size(SDL_Surface * surface)
 	int h=surface->h, w=surface->w;
 	Uint32 pixel;
 	Uint8 r,g,b;
-	for (int j=0; j<h ; j++)
+	int j=0;
+	for (j; j<h ; j++)
 	{
 		pixel=getpixel(surface, j,2);
 		SDL_GetRGB(pixel,surface->format,&r,&g,&b);
-		if (r==125)
+		if (r==255)
 			break;
 	}
 	int mod_size=0;
@@ -27,21 +28,29 @@ int module_size(SDL_Surface * surface)
 	return mod_size;
 }
 
-int QR(SDL_Surface *surface, int mod_size)
+int** QR(SDL_Surface *surface, int mod_size)
 {
 	int h=surface->h, w=surface->w;
 	Uint32 pixel;
 	Uint8 r,g,b;
-	int qr_size=0;
-	for (int i=0; i<h ; i+=mod_size)
-		qr_size+=i;
-	mod mod[qr_size][qr_size];
-	qr *qr;
+	int mat[(int)(w/mod_size)][(int)(w/mod_size)];
+	int ix =0;
+	int iy =0;
+	for (int i=mod_size; i<h ; i+=mod_size && ix++)
+	{	
+		for(int j=mod_size;j<h;j+=mod_size && iy++){
+			pixel = getpixel(surface,i-mod_size/2,j-mod_size/2);
+			SDL_GetRGB(pixel,surface->format,&r,&g,&b);
+			mat[ix][iy]= r;
+		}
+	}
+	/*qr *qr;
 	qr->matrix=mod;
 	qr->size=qr_size*qr_size;
 	qr->v=(qr_size-21)/4; //arithmetic formulas
+*/
 
-
+	
 	/*r (int i=0; i<h ; i++)
 	{
 		for (int j=0; j<w; j+=mod_size)
@@ -49,16 +58,21 @@ int QR(SDL_Surface *surface, int mod_size)
 			pixel=getpixel(surface,i,j);
 			SDL_GetRGB(pixel,surface->format,&r,&g,&b);
 			if (r==0)
-			{*/
-	return qr->v;
+			{
+	return qr->v;*/
+
+	return mat;
 }
 
-int main (int argc, char **argv )
+int main (int argc, char *argv[])
 {
 	if (argc!=2)
 		errx(1,"too many arguments");
 	init_sdl();
-	SDL_image *qr=load_image(argv[1]);
-	int mod_size=module_size(qr);
+	SDL_Surface *qr=load_image(argv[1]);
+	int size = module_size(qr);
 
+	int** data = QR(qr,size);
 
+	return 0;
+}
