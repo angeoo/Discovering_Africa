c1f8e2b4f8ba3384bc7cd23915d2e2e2cbd6f091
Ange <amg-marie-adingra.konian@epita.fr>
2023-03-20 15:56:41 +0100

    correction to masking

--- src/decode/masking.h
+++ src/decode/masking.h
@@ -1,8 +1,5 @@
 #ifndef MASKING_H
 #define MASKING_H
-/*#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>*/
 
 
 char * Find_masking(int *data,int size);
--- src/decode/utils.h
+++ src/decode/utils.h
@@ -36,6 +36,7 @@ Uint32 getpixel(SDL_Surface *surface, int x, int y)
 			return 0; 
 	}
 }
+
 void putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel)
 {
 	int bpp = surface->format->BytesPerPixel;
