afd5ed12e104ab6011ffb23663605490f2ff8e58
Ange <amg-marie-adingra.konian@epita.fr>
2023-05-17 19:27:22 +0200

    architecture to do

---
+++ src/decode/initQR.c
@@ -0,0 +1,151 @@
+#include "initQR.h"
+
+int module_size(SDL_Surface * surface)
+{
+    int h=surface->h, w=surface->w;
+    Uint32 pixel;
+    Uint8 r,g,b;
+    int j=0;
+    for (; j<h ; j++)
+    {
+        pixel=getpixel(surface, 2,j);
+        SDL_GetRGB(pixel,surface->format,&r,&g,&b);
+        if (r==255)
+            break;
+    }
+    int mod_size=0;
+    for(int k=0;k<w;k++)
+    {
+        pixel=getpixel(surface,k,j/2);
+        SDL_GetRGB(pixel,surface->format,&r,&g,&b);
+        if (r==255)
+        {
+            mod_size=k;
+            break;
+        }
+    }
+    return mod_size;
+}
+
+
+
+int* QR(SDL_Surface *surface, int mod_size)
+{
+    int h=surface->h, w=surface->w;
+    Uint32 pixel;
+    Uint8 r,g,b;
+    int size=(int)(w/mod_size)*(int)(w/mod_size);
+    int* mat=calloc(size,sizeof(int));
+
+    int ix=0;
+    for (int i=mod_size/2; i<h && ix<(w/mod_size) ; i+=mod_size )
+    {
+        int iy=0;
+        for (int j=mod_size/2; j<w && iy<(w/mod_size); j+=mod_size )
+        {
+            pixel=getpixel(surface,i,j);
+            SDL_GetRGB(pixel,surface->format,&r,&g,&b);
+            if (r==0){
+                *(mat+(iy*(w/mod_size)+ix))=1;
+            }
+            iy++;
+        }
+        ix++;
+    }
+
+    return mat;
+}
+
+
+int* mat_data (int* matrice , size_t taille )
+{
+    int count=0;
+    int* res = calloc(taille*taille, sizeof(int));
+    if (taille<22)
+    {
+        for (size_t x = 0 ; x<taille ; x +=1)
+        {
+            for (size_t y = 0; y<taille ; y+=1)
+            {
+                res[x*taille+y]=matrice[x*taille+y];
+                if (((x<8)&&(y<8))||((x<8)&&(y>=taille-8))||((x>=taille-8)&&(y<8)))
+                {
+                    res[x*taille+y]=-1;
+                }
+                if (((((x>7)&&(x<taille-8))&& y==6) ||(((y>7)&&(y<taille-8))&& x==6 )) || (x==taille-8 && y==8))
+                {
+                    res[x*taille+y]=-2;
+                    count+=1;
+                }
+                if(((x==8)&&(( y<=8 && y!=6) || (y>=taille-8))) || ((y==8)&&(x>=taille-7 || x<=8)&& x!=6))
+                {
+                    res[x*taille + y]=-3;
+                }
+
+
+
+            }
+
+        }
+    }
+    else
+    {
+
+        for (size_t x = 0 ; x<taille ; x +=1)
+        {
+            for (size_t y = 0; y<taille ; y+=1)
+            {
+
+                res[x*taille+y]=matrice[x*taille+y];
+                if (((x<8)&&(y<8))||((x<8)&&(y>=taille-8))||((x>=taille-8)&&(y<8)))
+                {
+                    res[x*taille+y]=-1;
+                }
+                if (((((x>7)&&(x<taille-8))&& y==6) ||(((y>7)&&(y<taille-8))&& x==6 ))||(((x>=taille-9)&&(x<taille-4))&&((y>=taille-9)&&(y<taille-4)))|| (x==taille-8 && y==8))
+                {
+                    res[x*taille+y]=-2;
+                    count+=1;
+                }
+                if(((x==8)&&(( y<=8 && y!=6) || (y>=taille-8))) || ((y==8)&&(x>=taille-7 || x<=8)&& x!=6))
+                {
+                    res[x*taille + y]=-3;
+                }
+
+
+            }
+
+        }
+    }
+
+    return res;
+
+}
+
+int main (int argc, char *argv[])
+{
+
+    init_sdl();
+    if (argc!=2)
+    {
+        errx(1,"arguments not 2\n");
+    }
+    SDL_Surface *qr=load_image(argv[1]);
+    int w=qr->w;
+    int size=module_size(qr);
+    int *data=QR(qr,size);
+    int *data_matrix=mat_data(data,w/size);
+    for (int i=0; i<w; i++){
+	    for (int j=0;j<size;j++){
+		    if (data_matrix[i*size+j]<0)
+			    printf("%d |",data_matrix[i*size+j]);
+		    else
+			    printf(" %d |",data_matrix[i*size+j]);
+		    if (w%i==0)
+			    printf("\n");
+	    }
+    }
+    free(data);
+    free(data_matrix);
+
+    return 0;
+}
---
+++ src/decode/initQR.h
@@ -0,0 +1,120 @@
+#ifndef INITQR_H
+#define INITQR_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <err.h>
+#include <string.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+int* mat_data (int* matrice , size_t taille );
+int* QR(SDL_Surface *surface, int mod_size);
+int module_size(SDL_Surface * surface);
+
+Uint32 getpixel(SDL_Surface *surface, int x, int y)
+{
+	int bpp = surface->format->BytesPerPixel;
+	/* Here p is the address to the pixel we want to retrieve */
+	Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
+
+	switch(bpp) {
+		case 1:
+			return *p;
+
+		case 2:
+			return *(Uint16 *)p;
+
+		case 3:
+			if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+				return p[0] << 16 | p[1] << 8 | p[2];
+			else
+				return p[0] | p[1] << 8 | p[2] << 16;
+
+		case 4:
+			return *(Uint32 *)p;
+
+		default:
+			return 0; 
+	}
+}
+
+void putpixel(SDL_Surface *surface, int x, int y, Uint32 pixel)
+{
+	int bpp = surface->format->BytesPerPixel;
+	/* Here p is the address to the pixel we want to set */
+	Uint8 *p = (Uint8 *)surface->pixels + y * surface->pitch + x * bpp;
+
+	switch(bpp) {
+		case 1:
+			*p = pixel;
+			break;
+
+		case 2:
+			*(Uint16 *)p = pixel;
+			break;
+
+		case 3:
+			if(SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+				p[0] = (pixel >> 16) & 0xff;
+				p[1] = (pixel >> 8) & 0xff;
+				p[2] = pixel & 0xff;
+			} else {
+				p[0] = pixel & 0xff;
+				p[1] = (pixel >> 8) & 0xff;
+				p[2] = (pixel >> 16) & 0xff;
+			}
+			break;
+
+		case 4:
+			*(Uint32 *)p = pixel;
+			break;
+	}
+}
+
+void init_sdl()
+{
+    // Init only the video part.
+    // If it fails, die with an error message.
+    if(SDL_Init(SDL_INIT_VIDEO) == -1)
+        errx(1,"Could not initialize SDL: %s.\n", SDL_GetError());
+}
+
+SDL_Surface* load_image(char *path)
+{
+    SDL_Surface *img;
+
+    // Load an image using SDL_image with format detection.
+    // If it fails, die with an error message.
+    img = IMG_Load(path);
+    if (!img)
+        errx(3, "can't load %s: %s", path, IMG_GetError());
+
+    return img;
+}
+
+SDL_Surface* display_image(SDL_Surface *img)
+{
+    SDL_Surface *screen;
+
+    // Set the window to the same size as the image
+    screen = SDL_SetVideoMode(img->w, img->h, 0, SDL_SWSURFACE|SDL_ANYFORMAT);
+    if (screen == NULL)
+    {
+        // error management
+        errx(1, "Couldn't set %dx%d video mode: %s\n",
+                img->w, img->h, SDL_GetError());
+    }
+
+    // Blit onto the screen surface
+    if(SDL_BlitSurface(img, NULL, screen, NULL) < 0)
+        warnx("BlitSurface error: %s\n", SDL_GetError());
+
+    // Update the screen
+    SDL_UpdateRect(screen, 0, 0, img->w, img->h);
+
+    // return the screen for further uses
+    return screen;
+}
+
+#endif
